c623e3686dc1956dcb125fe031d6574ad0cda80b
rghart <rhart@lyft.com>
2020-09-19 14:55:48 -0500

    Adding searching features and style

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
-const SearchBar = ({ children, searchText, updateSearchText }) => {
+const SearchBar = ({ children, leagueID, updateLeagueID, getLeagueData }) => {
   return (
     <div className="search-box">
       <div>
         {children}
       </div>
       <div>
-        <input type="text" value={searchText} onChange={updateSearchText} placeholder="Search Players" />
+        <input type="text" value={leagueID} onChange={updateLeagueID} placeholder="Update league ID" />
+        <button onClick={getLeagueData}>
+            Submit
+        </button>
       </div>
     </div>
   );
