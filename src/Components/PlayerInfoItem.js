4da8d8a743a148d1bf794e2d6042296dbba9b441
Ryan Hart <rhart@lyft.com>
2021-04-01 10:49:59 -0500

    Style fixes

--- src/Components/PlayerInfoItem.js
+++ src/Components/PlayerInfoItem.js
@@ -45,7 +45,7 @@ const PlayerInfoItem = ({ player, playerInfo, addToRoster, rankingPlayersIdsList
                 <div className="player-info">
                     <p className="player-info-item"><b>Manager:</b> {player.rostered_by ? player.rostered_by : "Free Agent"}</p>
                     <p className="player-info-item"><b>Rank:</b> {rankPlayerId.ranking}</p>
-                    <div className="avatar-player" aria-label="nfl Player" style={{width: 32 + "px", height: 32 + "px", flex: "0 0 32 px", background: `url(https://sleepercdn.com/content/nfl/players/thumb/${player.player_id}.jpg) center center / cover rgb(239, 239, 239)`, borderRadius: 33 + "%", marginTop: -3 + "%", backgroundColor: "transparent"}}></div>
+                    <div className="avatar-player" aria-label="nfl Player" style={{width: 32 + "px", height: 32 + "px", flex: "0 0 32 px", background: `url(https://sleepercdn.com/content/nfl/players/thumb/${player.player_id}.jpg) center center / cover rgb(239, 239, 239)`, borderRadius: 33 + "%", marginTop: -3 + "%", marginLeft: 30 + "px", backgroundColor: "transparent"}}></div>
                 </div>
             </div>
             {
