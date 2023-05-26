7b39fbe0375217b0188893e854023192296a2474
Ryan Hart <ryanghart91@gmail.com>
2023-05-26 12:50:09 -0500

    Fix prettier errors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {createRoot} from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const container = document.getElementById('root')
-const root = createRoot(container)
+const container = document.getElementById('root');
+const root = createRoot(container);
 
 root.render(
     <React.StrictMode>
